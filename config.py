199c4e11a94bc650945408a3607f0a7e22479e69
3mmvnu3l <emm.moreno@duocuc.cl>
2026-06-22 22:09:07 -0400

    implementacion de sqlite

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from typing import Optional
 class Settings:
     """Configuración de la aplicación"""
     
-    DATABASE_URL: Optional[str] = os.getenv("DATABASE_URL")
+    DATABASE_URL: Optional[str] = os.getenv("DATABASE_URL", "sqlite:///./data_science.db")
     APP_NAME: str = "API de Ciencia de Datos"
     APP_VERSION: str = "1.0.0"
-    APP_DESCRIPTION: str = "API para análisis de datos con FastAPI y PostgreSQL"
+    APP_DESCRIPTION: str = "API para análisis de datos con FastAPI"
 
 
 settings = Settings()
